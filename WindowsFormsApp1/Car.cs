fe2948b78f793a3bc97930d3ac90ae7292071b67
InitialCommit <torabi46@gmail.com>
2018-04-04 17:52:14 +0430

    Adding the Car class and use in the IK branch

---
+++ WindowsFormsApp1/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Car:Object
+    {
+        public Car() : base()
+        {
+
+        }
+
+        public string name { get; set; }
+    }
+}
