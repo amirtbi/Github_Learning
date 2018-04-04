92d7234d49ef4533c92f1408f397d9e2a801b86a
InitialCommit <torabi46@gmail.com>
2018-04-04 17:57:20 +0430

    Adding the log class.

---
+++ WindowsFormsApp1/Log.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Log:Object
+    
+    {
+        public Log() : base()
+        {
+
+        }
+        public string Name { set; get; }
+    }
+
+   
+
+}
